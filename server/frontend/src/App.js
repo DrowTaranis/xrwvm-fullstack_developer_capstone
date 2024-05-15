bb19cc6a24ad0e551b3c87eef7f457db2f3ae3ee
DrowTaranis <olieclay@hotmail.com>
2024-05-15 14:28:14 -0400

    Create user management

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,15 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
